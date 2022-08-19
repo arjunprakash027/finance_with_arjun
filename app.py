be3d9923032131ee7a6287c4e841feef56fe4b34
Arjun P <72484657+arjunprakash027@users.noreply.github.com>
2022-08-19 23:08:07 +0530

    v1.0

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 from explore import finance_explore
+from footsy import footer
 
 finance_explore()
+footer()
---
+++ footsy.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+def footer():
+    footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: transparent;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://arjunprakash027.github.io/" target="_blank">Arjun</a></p>
+</div>
+"""
+    st.markdown(footer,unsafe_allow_html=True)
